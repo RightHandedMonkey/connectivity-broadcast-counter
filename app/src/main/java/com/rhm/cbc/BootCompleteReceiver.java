c2877521800cd4e7bdb9cc94e130c080fdab6925
RightHandedMonkey <sam.bossen@gmail.com>
2017-11-11 12:29:53 -0500

    Corrected permission of BootComplete action

--- app/src/main/java/com/rhm/cbc/BootCompleteReceiver.java
+++ app/src/main/java/com/rhm/cbc/BootCompleteReceiver.java
@@ -5,16 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import com.rhm.cbc.CBCApplication;
-
 /**
  * Created by sambo on 11/11/17.
  */
 
 public class BootCompleteReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        //This is just a placeholder to make sure the application is alive so that
+        //Connectivity actions are registered
         Log.d("SAMB", "BootCompleteReceiver event received. App: "+ CBCApplication.class.getName()+ " Should have started");
-
     }
 }
